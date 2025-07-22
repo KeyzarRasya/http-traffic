b21952f78859e6fe694dd7d3a9c746c776ad3235
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-22 23:44:40 +0700

    feat: changing linkedlist value type to generic for request and response struct

--- include/linked_list.h
+++ include/linked_list.h
@@ -3,22 +3,36 @@
 
 #include <stdio.h>
 #include "http_request.h"
+#include "http_response.h"
+
+enum linkedlist_type {
+	LL_HttpRequest,
+	LL_HttpResponse,
+};
+
+union node_value {
+	struct http_request *req;
+	struct http_response *res;
+};
 
 struct linkedlist {
 	struct node *first;
 	struct node *last;
 	size_t size;
+	enum linkedlist_type type;
 };
 
 struct node {
 	struct node *prev;
-	struct http_request *val;
+	union node_value *val;
 	struct node *next;
 };
 
-void init_linkedlist(struct linkedlist *ll);
-struct node *init_node(struct node *prev, struct http_request *val, struct node *next);
-void add_last(struct linkedlist *ll, struct http_request *val);
+struct linkedlist *init_linkedlist(enum linkedlist_type type);
+struct node *init_node(struct node *prev, union node_value *val, struct node *next);
+void add_last(struct linkedlist *ll, union node_value *val);
 void print_linkedlist(struct linkedlist *ll);
+void print_linkedlist_request(struct linkedlist *ll);
+void print_linkedlist_response(struct linkedlist *ll);
 
 #endif
