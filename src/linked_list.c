94d55649f9cfe228d431a80a5d83ecd287fe8954
KeyzarRasya <keyzar.r.athallah@gmail.com>
2025-07-22 23:45:12 +0700

    feat: implementing generic type for request and response in linkedlist data structure

--- src/linked_list.c
+++ src/linked_list.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include "../include/linked_list.h"
 
-void init_linkedlist(struct linkedlist *ll) {
-	ll = malloc(sizeof(struct linkedlist));
+struct linkedlist *init_linkedlist(enum linkedlist_type type) {
+	struct linkedlist *ll = malloc(sizeof(struct linkedlist));
 	if (!ll) {
 		perror("Failed to allocate linkedlist");
-		return;
+		return NULL;
 	}
 	ll->first = NULL;
 	ll->last = NULL;
 	ll->size = 0;
+	ll->type = type;
+
+	return ll;
 }
 
 struct node *init_node(
 	struct node *prev,
-	struct http_request *val,
+	union node_value *val,
 	struct node *next
 ) {
 	struct node *n = malloc(sizeof(struct node));
@@ -23,10 +26,11 @@ struct node *init_node(
 	n->prev = prev;
 	n->next = next;
 	n->val = val;
+
 	return n;
 }
 
-void add_last(struct linkedlist *ll, struct http_request *val) {
+void add_last(struct linkedlist *ll, union node_value *val) {
 	struct node *n = init_node(ll->last, val, NULL);
 	if (!n) return; 
 	if (ll->first == NULL) {
@@ -40,9 +44,30 @@ void add_last(struct linkedlist *ll, struct http_request *val) {
 }
 
 void print_linkedlist(struct linkedlist *ll) {
+	switch (ll->type) {
+		case LL_HttpRequest:
+			print_linkedlist_request(ll);
+			break;
+		case LL_HttpResponse:
+			print_linkedlist_response(ll);
+			break;
+	}
+}
+
+void print_linkedlist_request(struct linkedlist *ll) {
 	struct node *current_node = ll->first;
+
 	while (current_node != NULL) {
-		printf("%s\n", current_node->val->url);
+		printf("%s\n", current_node->val->req->url);
+		current_node = current_node->next;
+	}
+}
+
+void print_linkedlist_response(struct linkedlist *ll) {
+	struct node *current_node = ll->first;
+
+	while (current_node != NULL) {
+		printf("%d\n", current_node->val->res->status);
 		current_node = current_node->next;
 	}
 }
